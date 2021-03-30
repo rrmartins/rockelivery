208b596da3d4582fed869bd5a4f3d019c4845ded
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted lib/rockelivery/users/delete.ex file

--- lib/rockelivery/users/delete.ex
+++ lib/rockelivery/users/delete.ex
@@ -3,7 +3,7 @@ defmodule Rockelivery.Users.Delete do
 
   def call(id) do
     case Repo.get(User, id) do
-      nil -> {:error, Error.build_user_not_found_error }
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> Repo.delete(user)
     end
   end

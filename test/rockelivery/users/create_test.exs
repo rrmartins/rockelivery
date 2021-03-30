07dce38c3c500a0491e45f272135c2bbbf2ce162
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 22:03:07 +0100

    add new tests from create user

---
+++ test/rockelivery/users/create_test.exs
@@ -0,0 +1,29 @@
+defmodule Rockelivery.Users.CreateTest do
+  use Rockelivery.DataCase, async: true
+
+  alias Rockelivery.{Error, User}
+  alias Rockelivery.Users.Create
+
+  import Rockelivery.Factory
+
+  describe "call/1" do
+    test "when all params are valid, returns the user" do
+      params = build(:user_params)
+
+      response = Create.call(params)
+
+      assert {:ok, %User{id: _id, age: 32, email: "rrmartinsjg@gmail.com"}} = response
+    end
+
+    test "when there are invalid params, returns an error" do
+      params = build(:user_params, %{password: "123", age: 13})
+
+      response = Create.call(params)
+
+      expected_response = %{age: ["must be greater than or equal to 18"], password: ["should be at least 6 character(s)"]}
+
+      assert {:error, %Error{status: :bad_request, result: changeset}} = response
+      assert errors_on(changeset) == expected_response
+    end
+  end
+end

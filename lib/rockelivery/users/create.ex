808ae059110a3d6879a241dba30af8efd53cffec
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted lib/rockelivery/users/create.ex file

--- lib/rockelivery/users/create.ex
+++ lib/rockelivery/users/create.ex
@@ -3,12 +3,13 @@ defmodule Rockelivery.Users.Create do
 
   def call(params) do
     params
-    |> User.changeset
-    |> Repo.insert
+    |> User.changeset()
+    |> Repo.insert()
     |> handle_insert
   end
 
   defp handle_insert({:ok, %User{} = result}), do: {:ok, result}
+
   defp handle_insert({:error, result}) do
     {:error, Error.build(:bad_request, result)}
   end

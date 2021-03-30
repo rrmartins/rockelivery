052a83b45b069587414cdadd94fdc6849112794c
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted test/rockelivery_web/controllers/users_controller_test.exs file

--- test/rockelivery_web/controllers/users_controller_test.exs
+++ test/rockelivery_web/controllers/users_controller_test.exs
@@ -4,7 +4,6 @@ defmodule RockeliveryWeb.UsersControllerTest do
   import Rockelivery.Factory
 
   describe "create/2" do
-
     test "when all params are valid, creates the user", %{conn: conn} do
       params = %{
         "age" => 32,
@@ -16,11 +15,21 @@ defmodule RockeliveryWeb.UsersControllerTest do
         "name" => "Rodrigo Martins"
       }
 
-      response = conn
-      |> post(Routes.users_path(conn, :create, params))
-      |> json_response(:created)
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:created)
 
-      assert %{"message" => "User created", "user" => %{"address" => "Rua da pascoa", "age" => 32, "cpf" => "12312312312", "email" => "rrmartinsjg@gmail.com", "id" => _id}} = response
+      assert %{
+               "message" => "User created",
+               "user" => %{
+                 "address" => "Rua da pascoa",
+                 "age" => 32,
+                 "cpf" => "12312312312",
+                 "email" => "rrmartinsjg@gmail.com",
+                 "id" => _id
+               }
+             } = response
     end
 
     test "when there is some error, returns teh error", %{conn: conn} do
@@ -30,13 +39,20 @@ defmodule RockeliveryWeb.UsersControllerTest do
         "name" => "Rodrigo Martins"
       }
 
-      response = conn
-      |> post(Routes.users_path(conn, :create, params))
-      |> json_response(:bad_request)
+      response =
+        conn
+        |> post(Routes.users_path(conn, :create, params))
+        |> json_response(:bad_request)
 
       expected_response = %{
-              "message" => %{"address" => ["can't be blank"], "age" => ["must be greater than or equal to 18"], "cep" => ["can't be blank"], "cpf" => ["can't be blank"], "email" => ["can't be blank"]}
-            }
+        "message" => %{
+          "address" => ["can't be blank"],
+          "age" => ["must be greater than or equal to 18"],
+          "cep" => ["can't be blank"],
+          "cpf" => ["can't be blank"],
+          "email" => ["can't be blank"]
+        }
+      }
 
       assert expected_response == response
     end
@@ -47,9 +63,10 @@ defmodule RockeliveryWeb.UsersControllerTest do
       id = "7425ecb1-a6d4-4737-b2f9-89fae26b5a8b"
       insert(:user)
 
-      response = conn
-      |> delete(Routes.users_path(conn, :delete, id))
-      |> response(:no_content)
+      response =
+        conn
+        |> delete(Routes.users_path(conn, :delete, id))
+        |> response(:no_content)
 
       assert response == ""
     end

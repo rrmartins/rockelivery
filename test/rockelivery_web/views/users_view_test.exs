a6363fc778cf3f3d3d558b2cae87d983feb7d66b
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted test/rockelivery_web/views/users_view_test.exs file

--- test/rockelivery_web/views/users_view_test.exs
+++ test/rockelivery_web/views/users_view_test.exs
@@ -11,10 +11,21 @@ defmodule RockeliveryWeb.UsersViewTest do
     user = build(:user)
     response = render(UsersView, "create.json", user: user)
 
-    assert %{message: "User created", user: %User{
-      address: "Rua da pascoa", age: 32, cep: "12311231", cpf: "12312312312",
-      email: "rrmartinsjg@gmail.com", id: "7425ecb1-a6d4-4737-b2f9-89fae26b5a8b",
-      inserted_at: nil, name: "Rodrigo Martins",
-      password: "1234546", password_hash: nil, updated_at: nil}} = response
+    assert %{
+             message: "User created",
+             user: %User{
+               address: "Rua da pascoa",
+               age: 32,
+               cep: "12311231",
+               cpf: "12312312312",
+               email: "rrmartinsjg@gmail.com",
+               id: "7425ecb1-a6d4-4737-b2f9-89fae26b5a8b",
+               inserted_at: nil,
+               name: "Rodrigo Martins",
+               password: "1234546",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
   end
 end

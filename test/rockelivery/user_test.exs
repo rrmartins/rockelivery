eeb1385ec21030232d16c7d4506811b45e550067
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted test/rockelivery/user_test.exs file

--- test/rockelivery/user_test.exs
+++ test/rockelivery/user_test.exs
@@ -23,9 +23,10 @@ defmodule Rockelivery.UserTest do
         password: "123456"
       }
 
-      response = params
-      |> User.changeset()
-      |> User.changeset(update_params)
+      response =
+        params
+        |> User.changeset()
+        |> User.changeset(update_params)
 
       assert %Changeset{changes: %{name: "João da Massa"}, valid?: true} = response
     end
@@ -35,11 +36,13 @@ defmodule Rockelivery.UserTest do
 
       response = User.changeset(params)
 
-      expected_response = %{age: ["must be greater than or equal to 18"], cpf: ["should be 11 character(s)"], password: ["should be at least 6 character(s)"]}
+      expected_response = %{
+        age: ["must be greater than or equal to 18"],
+        cpf: ["should be 11 character(s)"],
+        password: ["should be at least 6 character(s)"]
+      }
 
       assert errors_on(response) == expected_response
     end
   end
-
-
 end

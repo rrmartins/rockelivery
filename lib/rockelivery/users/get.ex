7247446b3ab911bb63980264534584720954ad28
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-03-30 23:21:55 +0100

    formatted lib/rockelivery/users/get.ex file

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -3,7 +3,7 @@ defmodule Rockelivery.Users.Get do
 
   def by_id(id) do
     case Repo.get(User, id) do
-      nil -> {:error, Error.build_user_not_found_error}
+      nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
     end
   end

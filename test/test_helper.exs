e2edfa980878de5afacea164e5c021745b7265be
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-23 23:50:48 +0100

    add config from mox

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,5 @@
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(Rockelivery.Repo, :manual)
 {:ok, _} = Application.ensure_all_started(:ex_machina)
+
+Mox.defmock(Rockelivery.ViaCep.ClientMock, for: Rockelivery.ViaCep.Behaviour)

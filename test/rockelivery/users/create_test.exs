3a9fcf986038c42cca9adfcb6c75cdb0b02db958
Rodrigo Martins <rrmartinsjg@gmail.com>
2021-04-23 23:50:48 +0100

    add mox to mock test

--- test/rockelivery/users/create_test.exs
+++ test/rockelivery/users/create_test.exs
@@ -3,12 +3,18 @@ defmodule Rockelivery.Users.CreateTest do
 
   alias Rockelivery.{Error, User}
   alias Rockelivery.Users.Create
+  alias Rockelivery.ViaCep.ClientMock
 
+  import Mox
   import Rockelivery.Factory
 
   describe "call/1" do
     test "when all params are valid, returns the user" do
       params = build(:user_params)
+
+      expect(ClientMock, :get_cep_info, fn _cep ->
+        {:ok, build(:cep_info)}
+      end)
 
       response = Create.call(params)
 
@@ -16,7 +22,7 @@ defmodule Rockelivery.Users.CreateTest do
     end
 
     test "when there are invalid params, returns an error" do
-      params = build(:user_params, %{password: "123", age: 13})
+      params = build(:user_params, %{"password" => "123", "age" => 13})
 
       response = Create.call(params)
 
